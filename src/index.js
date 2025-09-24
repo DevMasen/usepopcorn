2aff441f1ccb87da0ee13d2f71c37f09835301b9
DevMasen <mohammadwh400@gmail.com>
2025-09-24 12:11:31 +0330

    feat: E119: some random changes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		{/* <App /> */ <StarRating maxStars={10} />}
+		{/* <App /> */}
+		<StarRating maxStars={10} />
 	</React.StrictMode>
 );
