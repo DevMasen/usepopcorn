bec87f0e0359f7f82cb7ca5b837747da03a4127b
DevMasen <mohammadwh400@gmail.com>
2025-09-27 14:53:31 +0330

    excersize: testing StarRating component

--- src/StarRating.js
+++ src/StarRating.js
@@ -10,20 +10,30 @@ const starsContainerStyle = {
 	display: 'flex',
 };
 
-const textStyle = {
-	lineHeight: '1',
-	margin: '0',
-	fontSize: '21px',
-	fontFamily:
-		"-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
-	fontWeight: '500',
-};
+export default function StarRating({
+	maxStars = 5,
+	color = '#fcc419',
+	size = 48,
+	className = '',
+	messages = [],
+	defaultRating = 0,
+	onMovieRating = function (index = 0) {},
+}) {
+	const textStyle = {
+		lineHeight: '1',
+		margin: '0',
+		fontSize: `${size / 1.5}px`,
+		fontFamily:
+			"-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+		fontWeight: '500',
+		color,
+	};
 
-export default function StarRating({ maxStars = 5 }) {
-	const [rating, setRating] = useState(0);
+	const [rating, setRating] = useState(defaultRating);
 	const [tempRating, setTempRating] = useState(0);
 	function handleRating(index) {
 		setRating(index);
+		onMovieRating(index);
 	}
 	function handleMouseEnter(index) {
 		setTempRating(index);
@@ -32,7 +42,7 @@ export default function StarRating({ maxStars = 5 }) {
 		setTempRating(0);
 	}
 	return (
-		<div style={starRatingContainerStyle}>
+		<div style={starRatingContainerStyle} className={className}>
 			<div style={starsContainerStyle}>
 				{Array.from({ length: maxStars }, (_, i) => (
 					<Star
@@ -40,25 +50,31 @@ export default function StarRating({ maxStars = 5 }) {
 						full={
 							tempRating ? tempRating >= i + 1 : rating >= i + 1
 						}
+						color={color}
+						size={size}
 						onRate={() => handleRating(i + 1)}
 						onMouseEnter={() => handleMouseEnter(i + 1)}
 						onMouseLeave={handleMouseLeave}
 					/>
 				))}
 			</div>
-			<p style={textStyle}>{tempRating || rating || ''}</p>
+			<p style={textStyle}>
+				{messages.length === maxStars
+					? messages[tempRating ? tempRating - 1 : rating - 1]
+					: tempRating || rating || ''}
+			</p>
 		</div>
 	);
 }
 
-const starStyle = {
-	width: '42px',
-	height: '42px',
-	cursor: 'pointer',
-	display: 'block',
-};
+function Star({ full, onRate, onMouseEnter, onMouseLeave, color, size }) {
+	const starStyle = {
+		width: `${size}px`,
+		height: `${size}px`,
+		cursor: 'pointer',
+		display: 'block',
+	};
 
-function Star({ full, onRate, onMouseEnter, onMouseLeave }) {
 	return (
 		<span
 			role="button"
@@ -71,8 +87,8 @@ function Star({ full, onRate, onMouseEnter, onMouseLeave }) {
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 20 20"
-					fill="#000"
-					stroke="#000"
+					fill={color}
+					stroke={color}
 				>
 					<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
 				</svg>
@@ -81,7 +97,7 @@ function Star({ full, onRate, onMouseEnter, onMouseLeave }) {
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
 					viewBox="0 0 24 24"
-					stroke="#000"
+					stroke={color}
 				>
 					<path
 						strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './StarRating';
+import './index.css';
+import App from './App';
+// import StarRating from './StarRating';
+// import './test.css';
+
+// function Test() {
+// 	const [movieRating, setMovieRating] = useState(0);
+// 	return (
+// 		<div>
+// 			<StarRating color="#ccc" onMovieRating={setMovieRating} />
+// 			<p>The movie rating is : {movieRating}</p>
+// 		</div>
+// 	);
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		{/* <App /> */}
+		<App />
+		{/* <StarRating />
 		<StarRating maxStars={10} />
+		<StarRating maxStars={15} color="red" />
+		<StarRating maxStars={5} color="purple" size={22} />
+		<StarRating
+			maxStars={5}
+			color="white"
+			size={22}
+			className="black-background border-radius-1rem width-fitcontent padding-1rem"
+		/>
+		<StarRating
+			maxStars={5}
+			messages={['Terrible', 'OK', 'Good', 'Nice', 'Amazing']}
+			defaultRating={1}
+		/>
+		<Test /> */}
 	</React.StrictMode>
 );
