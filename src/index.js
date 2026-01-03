b354fce30a4f88e0a073df2dd5161c8a65e13969
DevMasen <mohammadwh400@gmail.com>
2026-01-03 16:03:01 +0330

    S12: E141: chore: Documentation added to the source

--- src/index.js
+++ src/index.js
@@ -37,3 +37,12 @@ root.render(
 		{/* <Test /> */}
 	</React.StrictMode>
 );
+
+//! Documents
+
+/*
+Component Instance lifecycle:
+1: Mount (initial render)
+2: Re-render (state changes, Props change, Parent re-renders, Context changes)
+3: Unmount
+*/
